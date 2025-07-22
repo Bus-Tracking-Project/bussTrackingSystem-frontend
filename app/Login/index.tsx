3d6646cb4650433ff8f09860227cd981741abe95
Vemunuri Vivek <srinivasvemunuri8@email.com>
2025-07-22 16:20:48 +0530

    edited login page

--- app/Login/index.tsx
+++ app/Login/index.tsx
@@ -1,20 +1,20 @@
-import { View, Text, TextInput, Pressable, Alert } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons';
-import { useState } from 'react';
 import { Link, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { Alert, Pressable, Text, TextInput, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
   const router = useRouter(); // <--- and this
-  const [emailOrPhone, setEmailOrPhone] = useState('');
+  const [Phone, setPhone] = useState('');
   const [password, setPassword] = useState('');
 
   const validateForm = () => {
-    const isEmail = emailOrPhone.includes('@') && emailOrPhone.endsWith('.com');
-    const isPhone = /^[0-9]{10}$/.test(emailOrPhone);
+    const isEmail = Phone.includes('@') && Phone.endsWith('.com');
+    const isPhone = /^[0-9]{10}$/.test(Phone);
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
-    if (!emailOrPhone || !password) {
+    if (!Phone || !password) {
       Alert.alert('Missing Fields', 'Please fill in all fields.');
       return false;
     }
@@ -39,7 +39,7 @@ export default function LoginScreen() {
     if (!validateForm()) return;
 
     const payload = {
-      username: emailOrPhone,
+      username: Phone,
       password: password,
     };
 
@@ -66,56 +66,62 @@ export default function LoginScreen() {
 
 
     // Clear form after submission and console data
-    setEmailOrPhone('');
+    setPhone('');
     setPassword('');
     console.log(payload)
     router.navigate('../HomeScreen');
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-      <View className='flex-1 bg-white justify-center px-6'>
-        <Text className="text-3xl font-bold text-blue-600 mb-10 text-center">Login</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#1d1a23', }}>
+      <View className='bg-[#1d1a23] px-6 flex-1 flex-col justify-between'>
+        <View >
+          <Text className="text-4xl font-bold text-[#6644b5] my-10">Bus Yatra</Text>
+          <Text className='text-2xl font-bold text-gray-300 my-2'>Log in</Text>
+          <Text className='text-xl font-bold text-gray-500 my-2'>Already have a Bus yatra account? start with your mobile number</Text>
 
-        {/* Email or Phone Input */}
-        <View className="flex-row items-center border border-blue-400 rounded-xl px-3 py-2 mb-4">
-          <MaterialIcons name="email" size={24} color="#3B82F6" />
-          <TextInput
-            className="ml-3 flex-1 text-base  text-gray-300"
-            placeholder="Email or Phone Number"
-            value={emailOrPhone}
-            onChangeText={setEmailOrPhone}
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
+          {/* Phone Input */}
+          <Text className='text-gray-300 font-bold my-2'>Enter mobile number</Text>
+          <View className="flex-row items-center px-3 py-2 border-b-2 border-gray-600 my-2">
+            <MaterialIcons name="email" size={24} color="#6644b5" />
+            <TextInput
+              className="ml-3 flex-1 text-base  text-gray-300"
+              placeholder="Email or Phone Number"
+              value={Phone}
+              onChangeText={setPhone}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              placeholderTextColor="#9CA3AF"
+            />
+          </View>
+
+          {/* Password Input */}
+          <View className="flex-row items-center border-b-2 border-gray-600  px-3 py-2 mb-2">
+            <Ionicons name="lock-closed" size={24} color="#6644b5" />
+            <TextInput
+              className="ml-3 flex-1 text-base text-gray-300"
+              placeholderTextColor="#9CA3AF"
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+            />
+          </View>
+
+          {/* Forgot Password */}
+          <Text className="text-sm text-right text-[#6644b5] mb-6">Forgot Password?</Text>
+
         </View>
+        <View>
+          {/* Login Button */}
+          <Pressable onPress={handleLogin} className="bg-[#6644b5] py-4 rounded-3xl ">
+            <Text className="text-gray-300 text-center font-semibold text-lg">Login</Text>
+          </Pressable>
 
-        {/* Password Input */}
-        <View className="flex-row items-center border border-blue-400 rounded-xl px-3 py-2 mb-2">
-          <Ionicons name="lock-closed" size={24} color="#3B82F6" />
-          <TextInput
-            className="ml-3 flex-1 text-base text-gray-300"
-            placeholder="Password"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-          />
+          <Pressable onPress={() => router.back()} className="mt-4">
+            <Link href='/HomeScreen' className="text-[#6644b5] text-center text-base underline">continue without account?</Link>
+          </Pressable>
         </View>
-
-        {/* Forgot Password */}
-        <Text className="text-sm text-right text-blue-500 mb-6">Forgot Password?</Text>
-
-        {/* Login Button */}
-        <Pressable onPress={handleLogin} className="bg-blue-600 py-3 rounded-xl">
-          <Text className="text-white text-center font-semibold text-lg">Login</Text>
-        </Pressable>
-        {/* Already have account? Button */}
-        <Pressable
-          onPress={() => router.back()}
-          className="mt-4"
-        >
-          <Link href='/HomeScreen' className="text-blue-500 text-center text-base underline">continue without account?</Link>
-        </Pressable>
       </View>
     </SafeAreaView>
   );
